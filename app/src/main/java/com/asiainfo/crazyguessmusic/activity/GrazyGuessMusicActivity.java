fad35172836ee58343d651dc3a0e899474fbfd07
MicroKibaco <569015640@qq.com>
2017-01-26 11:29:38 +0800

    制作titleBar

---
+++ app/src/main/java/com/asiainfo/crazyguessmusic/activity/GrazyGuessMusicActivity.java
@@ -0,0 +1,15 @@
+package com.asiainfo.crazyguessmusic.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.asiainfo.crazyguessmusic.R;
+
+public class GrazyGuessMusicActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grazy_guess_music);
+    }
+}

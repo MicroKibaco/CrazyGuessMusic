5a795d7a237c50f289050396770b7571fba058f9
MicroKibaco <569015640@qq.com>
2017-01-31 22:12:18 +0800

    音乐播放器的封装

---
+++ app/src/main/java/com/asiainfo/crazyguessmusic/utils/MyPlayer.java
@@ -0,0 +1,69 @@
+package com.asiainfo.crazyguessmusic.utils;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
+
+/**
+ * @author MicroKibaco
+ * @date 1/31/17 21:52
+ * @Method 音乐播放器
+ * @description MyPlayer
+ */
+
+public class MyPlayer {
+
+
+    private static MyPlayer ourInstance = new MyPlayer();
+
+    //歌曲播放
+    private static MediaPlayer mMusicMediaPlayer;
+
+    private MyPlayer() {
+    }
+
+    private static void playSong(Context context, String fileName) {
+
+        if (mMusicMediaPlayer == null) {
+
+            mMusicMediaPlayer = new MediaPlayer();
+        }
+
+        //强制重置:
+        mMusicMediaPlayer.reset();
+
+        //加载声音文件
+        AssetManager assertmanager = context.getAssets();
+        try {
+            AssetFileDescriptor fileDescription = assertmanager.openFd(fileName);
+            mMusicMediaPlayer.setDataSource(fileDescription.getFileDescriptor(),
+                    fileDescription.getStartOffset(),
+                    fileDescription.getLength());
+
+            mMusicMediaPlayer.prepare();
+
+            //声音播放
+            mMusicMediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void stopTheSong(Context context) {
+
+        if (mMusicMediaPlayer != null) {
+
+            mMusicMediaPlayer.stop();
+
+        }
+
+    }
+
+    public static MyPlayer genstance() {
+        return ourInstance;
+    }
+}
